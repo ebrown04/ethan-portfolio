8ac0fd563ec96c54aa218384b1b5d9dba45bd7fd
Ethan Brown <ebrown0715@gmail.com>
2026-04-30 12:47:54 -0400

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import ProjectPage from "./pages/ProjectPage";
 
-export default function ScrollToTop() {
+function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
@@ -9,4 +11,16 @@ export default function ScrollToTop() {
   }, [pathname]);
 
   return null;
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects/:slug" element={<ProjectPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
